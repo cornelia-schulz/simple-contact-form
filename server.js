1ecdcf5a32b328580075e1398cee5505f36c39e1
David Walker <dave@Davids-MacBook-Pro.local>
2018-06-02 14:15:56 +1200

    contact form sends info to be displayed

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const server = express()
+const path = require('path')
 
+server.use(express.static(path.join(__dirname, 'public')))
 server.use(express.static('public'))
+server.use(express.urlencoded({extended: false}))
 
-
+server.post('/display', (req, res) => {
+  console.log(res)
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const email = req.body.email
+  res.send(`<p>First name: ${firstName}</p><p>Last name: ${lastName}</p><p>Email: ${email}</p>`)
+})
 
 module.exports = server
